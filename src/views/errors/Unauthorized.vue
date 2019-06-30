485844c9c88b8a426deb26138fb94ecce3dab380
Do Hoerin <hellodhlyn@gmail.com>
2019-07-01 00:46:40 +0900

    refreshing project

--- src/views/errors/Unauthorized.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div id="error-unauthorized">
-    <h1><ion-icon name="alert"></ion-icon> Unauthroized</h1>
-    <p>You don't have enough permissions to access. Please log in.</p>  
-  </div>
-</template>
--- vue.config.js
+++
@@ -1,4 +0,0 @@
-// See: https://cli.vuejs.org/config
-module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' ? '/web' : '/',
-};
