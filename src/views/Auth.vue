f43542a24cfa8cf90588828b1dd5aff5a3a59ee2
Do Hoerin <hellodhlyn@gmail.com>
2019-07-15 00:06:53 +0900

    Added add/remove permission view

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -4,21 +4,39 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'auth',
   created() {
     if (!this.$route.query.accessToken) {
       const endpoint = `${window.location.protocol}//${window.location.host}`;
       window.location = `https://auth.lynlab.co.kr/web/signin?appId=325fcc52-3c29-46b8-bb33-a30d20abf7c6&redirectUrl=${endpoint}/auth`;
-    } else {
-      this.$localStorage.set('auth.accessToken', this.$route.query.accessToken);
-      this.$localStorage.set('auth.refreshToken', this.$route.query.refreshToken);
-      this.$localStorage.set('auth.expireAt', this.$route.query.expireAt);
-
-      this.$store.commit('setAccessToken', this.$route.query.accessToken);
-
-      this.$router.push({ name: 'index' });
+      return;
     }
+
+    const { accessToken, refreshToken, expireAt } = this.$route.query;
+    const opts = {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    };
+
+    axios.get(`${process.env.VUE_APP_LUPPITER_HOST}/vulcan/auth/me`, opts)
+      .then(() => {
+        this.$localStorage.set('auth.accessToken', accessToken);
+        this.$localStorage.set('auth.refreshToken', refreshToken);
+        this.$localStorage.set('auth.expireAt', expireAt);
+
+        this.$store.commit('setAccessToken', accessToken);
+      })
+      .catch((e) => {
+        this.$notify({
+          group: 'luppiter',
+          title: '로그인에 실패했습니다.',
+          text: `Error ${e.response.status} (${e.response.data.error || e.response.data})`,
+          type: 'error',
+        });
+      })
+      .finally(() => this.$router.push({ name: 'index' }));
   },
 };
 </script>
